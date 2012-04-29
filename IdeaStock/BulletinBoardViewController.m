d621003a7df98922709a6ec6a5f050fdc009db50
Ali Fathalian <fatalian@uw.edu>
2012-04-28 22:44:35 -0700

    created empty bulletin board

--- IdeaStock/BulletinBoardViewController.m
+++ IdeaStock/BulletinBoardViewController.m
@@ -7,20 +7,31 @@
 //
 
 #import "BulletinBoardViewController.h"
-
+#import "DropBoxAssociativeBulletinBoard.h"
 
 @interface BulletinBoardViewController ()
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *label;
+@property (weak, nonatomic) IBOutlet UIScrollView *bulletinboardView;
+@property (strong, nonatomic) DropBoxAssociativeBulletinBoard * board;
 
 @end
 
 @implementation BulletinBoardViewController
 @synthesize label = _label;
+@synthesize bulletinboardView = _bulletinboardView; 
 @synthesize parent = _parent;
+@synthesize board = _board;
 
 @synthesize bulletinBoardName = _bulletinBoardName;
 
+- (DropBoxAssociativeBulletinBoard *) board{
+
+    if (!_board){
+        _board = [[DropBoxAssociativeBulletinBoard alloc] initBulletinBoardFromXoomlWithName:self.bulletinBoardName];
+    }
+    return _board;
+}
 - (void) setBulletinBoardName:(NSString *)bulletinBoardName{
     _bulletinBoardName = bulletinBoardName;
 
@@ -44,14 +55,22 @@
 - (void)viewDidLoad
 {
     
+    //load and display notes and groupings
+    //...
+    
     [super viewDidLoad];
     self.label.title = self.bulletinBoardName;
+    CGSize size =  CGSizeMake(self.bulletinboardView.bounds.size.width * 5, self.bulletinboardView.bounds.size.height * 5);
+   [self.bulletinboardView setContentSize:size];
+    
 	// Do any additional setup after loading the view.
 }
 
 - (void)viewDidUnload
 {
     [self setLabel:nil];
+    [self setView:nil];
+    [self setBulletinboardView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
