f3e20bdb18934a8577d812bc084b3b53d4608341
Ali Fathalian <fatalian@uw.edu>
2012-05-16 16:36:41 -0700

    removed layer view

--- IdeaStock/BulletinBoardViewController.m
+++ IdeaStock/BulletinBoardViewController.m
@@ -16,7 +16,6 @@
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *label;
 @property (weak, nonatomic) IBOutlet UIScrollView *bulletinboardView;
 @property (strong, nonatomic) DropBoxAssociativeBulletinBoard * board;
-@property (weak, nonatomic) UIView * layerView;
 @property (strong, nonatomic) NSArray * intersectingViews;
 @property int panCounter ;
 
@@ -28,7 +27,6 @@
 @synthesize bulletinboardView = _bulletinboardView; 
 @synthesize parent = _parent;
 @synthesize board = _board;
-@synthesize layerView = _layerView;
 @synthesize panCounter = _panCounter;
 @synthesize intersectingViews = _intersectingViews;
 
@@ -90,7 +88,7 @@
 
 - (NSArray *) checkForOverlapWithView: (UIView *) senderView{
     NSMutableArray * ans = [[NSMutableArray alloc] init];
-    for (UIView * view in self.layerView.subviews){
+    for (UIView * view in self.bulletinboardView.subviews){
         if ([view isKindOfClass: [NoteView class]] && view != senderView){
             if (CGRectIntersectsRect(view.frame,senderView.frame)){
                 [ans addObject:view];
@@ -127,7 +125,7 @@
                                      [mainNote removeFromSuperview];
                                      mainNote.alpha = 1;
                                      stack.alpha =0;
-                                     [self.layerView addSubview:stack];
+                                     [self.bulletinboardView addSubview:stack];
                                      [UIView animateWithDuration:0.5 animations:^{stack.alpha = 1;}];
                                      first = NO;
                                  
@@ -145,7 +143,7 @@
 -(void) notePanned: (UIPanGestureRecognizer *) sender{
     if( sender.state == UIGestureRecognizerStateChanged ||
        sender.state == UIGestureRecognizerStateEnded){
-        CGPoint translation = [sender translationInView:self.layerView];
+        CGPoint translation = [sender translationInView:self.bulletinboardView];
         self.panCounter++;
         if (self.panCounter > CHECK_TIME ){
             self.panCounter = 0;
@@ -168,7 +166,7 @@
         CGPoint newOrigin = CGPointMake(pannedView.frame.origin.x + translation.x,
                                         pannedView.frame.origin.y + translation.y);
         pannedView.frame = CGRectMake(newOrigin.x, newOrigin.y, pannedView.frame.size.width,pannedView.frame.size.height);
-        [sender setTranslation:CGPointZero inView:self.layerView];
+        [sender setTranslation:CGPointZero inView:self.bulletinboardView];
     }
     
     if (sender.state == UIGestureRecognizerStateEnded){
@@ -185,7 +183,7 @@
 }
 -(void) mainScreenDoubleTapped:(UITapGestureRecognizer *)sender{
     
-    CGPoint location = [sender locationOfTouch:0 inView:self.layerView];
+    CGPoint location = [sender locationOfTouch:0 inView:self.bulletinboardView];
     CGRect frame = CGRectMake(location.x, location.y, 200, 200);
     UIView * note = [[NoteView alloc] initWithFrame:frame];
     note.transform = CGAffineTransformScale(note.transform, 10, 10);
@@ -196,7 +194,7 @@
         note.alpha = 1;
     }];
     
-    [self.layerView addSubview:note];
+    [self.bulletinboardView addSubview:note];
     UIPanGestureRecognizer * gr = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(notePanned:)];
     UIPinchGestureRecognizer * pgr = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(notePinched:)];
     [note addGestureRecognizer:gr];
@@ -214,12 +212,7 @@
     self.label.title = self.bulletinBoardName;
     CGSize size =  CGSizeMake(self.bulletinboardView.bounds.size.width, self.bulletinboardView.bounds.size.height);
     [self.bulletinboardView setContentSize:size];
-    UIScrollView * layerView = [[UIScrollView alloc] initWithFrame:CGRectMake(self.bulletinboardView.bounds.origin.x,
-                                                                              self.bulletinboardView.bounds.origin.y,
-                                                                              size.width,
-                                                                              size.height)];
-    self.layerView = layerView;
-    [self.bulletinboardView addSubview:layerView];
+
     UITapGestureRecognizer * gr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mainScreenDoubleTapped:)];
     gr.numberOfTapsRequired = 2;
     
